b7ee530b4f888d51c556caf4131376140f2487f3
whalehappy <whalehappy@gmail.com>
2026-08-21 16:48:56 +0700

    fix main.js

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -72,7 +72,7 @@ $roleLabels = [
     'tech' => 'Kỹ thuật',
 ];
 
-$appVersion = 'v2.4.10';
+$appVersion = 'v2.4.11';
 ?>
 
 <nav class="sidebar-nav <?= AuthMiddleware::isMobile() ? 'mobile-sidebar-nav' : '' ?>">
