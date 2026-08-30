c6ffd3ab62cd44160c033b635520e4b9c42593cb
whalehappy <whalehappy@gmail.com>
2026-08-30 12:16:46 +0700

    update sidebar app version to v2.4.24

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -72,7 +72,7 @@ $roleLabels = [
     'tech' => 'Kỹ thuật',
 ];
 
-$appVersion = 'v2.4.22';
+$appVersion = 'v2.4.24';
 ?>
 
 <nav class="sidebar-nav <?= AuthMiddleware::isMobile() ? 'mobile-sidebar-nav' : '' ?>">
